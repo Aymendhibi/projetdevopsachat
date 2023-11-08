17d0ad3c4e533ef50ac67045db3d3e01d813481c
Mohamed Aziz AYADI <mohamedaziz.ayadi@esprit.tn>
2023-11-08 16:08:46 +0100

    nexus repo

--- src/test/java/FactureTest.java
+++ src/test/java/FactureTest.java
@@ -1,9 +1,17 @@
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.mock.mockito.MockBean;
+import tn.esprit.rh.achat.entities.Facture;
 import tn.esprit.rh.achat.repositories.FactureRepository;
 import tn.esprit.rh.achat.services.FactureServiceImpl;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import static org.junit.Assert.assertSame;
+import static org.junit.Assert.assertTrue;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
 
 
 public class FactureTest {
@@ -13,9 +21,15 @@ public class FactureTest {
     @Autowired
     private FactureServiceImpl factureService;
 
-    @Test
+  /*  @Test
     void testRetrieveAllFactures() {
+        ArrayList<Facture> factureList = new ArrayList<>();
+        when(factureRepository.findAll()).thenReturn(factureList);
+        List<Facture> actualRetrieveAllFacturesResult = factureService.retrieveAllFactures();
+        assertSame(factureList, actualRetrieveAllFacturesResult);
+        assertTrue(actualRetrieveAllFacturesResult.isEmpty());
+        verify(factureRepository).findAll();
 
 
-    }
+    }*/
 }
